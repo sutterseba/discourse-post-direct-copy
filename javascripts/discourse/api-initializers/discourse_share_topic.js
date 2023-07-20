78b1b86b07552c8de92bdd74e68bfcdf53160d67
chapoi <charlie@discourse.org>
2023-07-20 15:57:06 +0200

    add in animated checkmark

--- javascripts/discourse/api-initializers/discourse_share_topic.js
+++ javascripts/discourse/api-initializers/discourse_share_topic.js
@@ -12,19 +12,23 @@ export default apiInitializer("0.11.1", (api) => {
       return {
         action: "copyLink",
         icon: "d-post-share",
-        className: "copy-link",
+        className: "post-action-menu__copy-link",
         title: themePrefix("copy_title"),
         position,
       };
     });
 
     api.attachWidgetAction("post", "copyLink", function () {
-      let postUrl = this.attrs.shareUrl;
-      let shareUrl = new URL(postUrl, window.origin).toString();
-      let postId = this.attrs.id;
+      const postUrl = this.attrs.shareUrl;
+      const postId = this.attrs.id;
+      const shareUrl = new URL(postUrl, window.origin).toString();
+      const copyLinkBtn = document.querySelector(
+        `article[data-post-id='${postId}'] .post-action-menu__copy-link`
+      );
       navigator.clipboard.writeText(shareUrl).then(
         () => {
           createAlert("Link copied!", "success", postId);
+          createCheckmark(copyLinkBtn, postId);
         },
         () => {
           createAlert("Failed to copy link.", "fail");
@@ -51,6 +55,35 @@ function createAlert(message, type, postId) {
   }, 1500);
 
   setTimeout(function () {
-    document.body.removeChild(alertDiv);
+    document
+      .querySelector(`article[data-post-id='${postId}'] .post-menu-area`)
+      .removeChild(alertDiv);
   }, 2000);
+}
+
+function createCheckmark(btn, postId) {
+  let checkmark = makeSvg(postId);
+  btn.appendChild(checkmark);
+
+  setTimeout(() => {
+    document.getElementById(postId).classList.remove("is-visible");
+  }, 3000);
+
+  setTimeout(() => {
+    document
+      .getElementById(postId)
+      .parentNode.removeChild(document.getElementById(postId));
+  }, 3500);
+}
+
+function makeSvg(postId) {
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.setAttribute("class", "post-action-menu__copylink-checkmark is-visible");
+  svg.setAttribute("id", postId);
+  svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+  svg.setAttribute("viewBox", "0 0 52 52");
+  svg.innerHTML = `
+<path class="checkmark__check" fill="none" d="M13 26 l10 10 20 -20"/>
+      `;
+  return svg;
 }
